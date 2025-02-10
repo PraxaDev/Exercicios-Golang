50c14861291857e11d65f8d3899ff6361e832ab7
bruno <brunoo.praxedess@hotmail.com>
2025-02-10 13:42:21 -0300

    exercicio 5 completo

--- 05/main.go
+++ 05/main.go
@@ -1,2 +1,19 @@
 package main
 // Descrição: Crie um programa que gera a tabuada de um número escolhido pelo usuário.
+import("fmt")
+
+func main() {
+
+	num := 0
+
+	fmt.Printf("Digite um número da tabuada: ")
+	fmt.Scan(&num)
+	fmt.Printf("\nTabuada de %d\n", num)
+
+	for i := 1; i < 11; i++ {
+		result := num * i
+
+		fmt.Printf("%d x %d = %d\n", num, i, result)
+	}
+	
+}
