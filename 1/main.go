57662bec9312a762622fdcd7ecc823d2b48c5319
bruno <brunoo.praxedess@hotmail.com>
2025-02-09 23:44:42 -0300

    exercicio 1 completo

--- 1/main.go
+++ 1/main.go
@@ -1,2 +1,7 @@
 package main
 // Descrição: Crie um programa simples que imprime a mensagem "Olá, Mundo!" na tela. Este é geralmente o primeiro programa que as pessoas escrevem ao aprender uma nova linguagem de programação.
+import("fmt")
+
+func main () {
+	fmt.Println("Hello World")
+}
