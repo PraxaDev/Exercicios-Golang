72656dd693d05185b4bdbd3b1fecebbdba2a2b33
bruno <brunoo.praxedess@hotmail.com>
2025-02-11 14:12:27 -0300

    exercicio 6 completo

--- 06/main.go
+++ 06/main.go
@@ -1,2 +1,27 @@
 package main
 // Descrição: Desenvolva um programa que conta o número de vogais em uma palavra.
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	vogais := "aeiou"
+	var palavra string
+	contador := 0
+
+	fmt.Printf("Digite uma palavra: ")
+	fmt.Scan(&palavra)
+
+	palavra = strings.ToLower(palavra)
+
+	for _, letra := range palavra {
+		if strings.ContainsRune(vogais, letra) {
+			contador ++
+		}
+	}
+
+	fmt.Printf("A palavra '%s' tem %d vogais.\n", palavra, contador)
+}
+
